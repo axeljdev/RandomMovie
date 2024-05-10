2c4a64620e8277a43ee349f9507fccab1e98f149
AXEL JANIAK <axeljpro@gmail.com>
2024-05-10 16:37:10 +0200

    gitignore2

--- src/assets/components/Card.jsx
+++ src/assets/components/Card.jsx
@@ -4,7 +4,9 @@ import "/src/App.css"
 function Cards({randomNumber}) {
 
   const apiToken = import.meta.env.VITE_API_TOKEN
-  const [movie, setMovie] = useState([])
+  const [movie, setMovie] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   const options = {
     method: "GET",
@@ -14,19 +16,36 @@ function Cards({randomNumber}) {
     },
   };
 
-  useEffect(()=> {
-    
-    fetch(`https://api.themoviedb.org/3/movie/${randomNumber}?language=fr-FR`, options)
-      .then(response => response.json())
-      .then((data) => setMovie(data))
-      .catch(err => console.error(err));
-  });
+ useEffect(() => {
+  setIsLoading(true);
+  fetch(`https://api.themoviedb.org/3/movie/${randomNumber}?language=fr-FR`, options)
+    .then(response => {
+      // if (!response.ok) {
+      //   throw new Error('La requête a échoué.');
+      // }
+      return response.json();
+    })
+    .then((data) => {
+      setIsLoading(false);
+      setMovie(data);
+    })
+    .catch((error) => {
+      setIsLoading(false);
+      setError(error.message);
+    });
+}, [randomNumber]);
 
   return (
-    <article className="container" key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt={movie.title}/>
-            <h2>{movie.title}</h2>
-          </article>
+    <div>
+    {isLoading && <p>Chargement en cours...</p>}
+    {error}
+    {movie && (
+      <article className="container" key={movie.id}>
+        <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt={movie.title} />
+        <h2>{movie.title}</h2>
+      </article>
+    )}
+  </div>
   );
 }
 
